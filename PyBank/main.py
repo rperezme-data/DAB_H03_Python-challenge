87676ce59608075472b72674fb7519391aaec306
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-03-27 11:03:07 -0600

    Add Get header & Count months

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,13 +5,28 @@ import csv
 
 ## Define path for csv file
 csv_path = os.path.join('Resources','budget_data.csv')
-print(csv_path)
+#print(csv_path)
 
+## Set initial values
+month_count = 0
 
-with open(csv_path, newline='') as csv_file:
-    print(csv_file)
+##
+with open(csv_path, newline='', encoding='utf-8') as csv_file:
+    #print(csv_file)
     
+    ##
     csv_reader = csv.reader(csv_file, delimiter=',')
-    
+    print(csv_reader)
+
+    ## Get CSV header 
+    csv_header = next(csv_reader)
+    print(csv_header)
+
+    ## Loop through CSV rows
     for row in csv_reader:
-            print(row)
+            #print(row)
+
+            ## Count number of months (rows)
+            month_count += 1
+    
+    print (month_count)
