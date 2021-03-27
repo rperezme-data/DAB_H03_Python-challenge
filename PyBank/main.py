585f18abeec97ae618b5c7a6041a4357f718dd4e
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-03-27 11:35:30 -0600

    Add Sum Profits/Losses & Print Report

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,22 +5,23 @@ import csv
 
 ## Define path for csv file
 csv_path = os.path.join('Resources','budget_data.csv')
-#print(csv_path)
+# print(csv_path)
 
 ## Set initial values
 month_count = 0
+total_pnl = 0
 
-##
+## Read CSV file
 with open(csv_path, newline='', encoding='utf-8') as csv_file:
-    #print(csv_file)
+    # print(csv_file)
     
-    ##
+    ## Split data on commas
     csv_reader = csv.reader(csv_file, delimiter=',')
-    print(csv_reader)
+    # print(csv_reader)
 
     ## Get CSV header 
     csv_header = next(csv_reader)
-    print(csv_header)
+    # print(csv_header)
 
     ## Loop through CSV rows
     for row in csv_reader:
@@ -28,5 +29,14 @@ with open(csv_path, newline='', encoding='utf-8') as csv_file:
 
             ## Count number of months (rows)
             month_count += 1
+
+            ## Sum Profits/Losses
+            total_pnl += int(row[1])
     
-    print (month_count)
+
+    ## Print Analysis Results
+    print()
+    print("Financial Analysis")
+    print("------------------------------")
+    print (f"Total months: {month_count}")
+    print (f"Total: ${total_pnl:,}")
