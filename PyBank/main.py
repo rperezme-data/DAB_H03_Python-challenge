14cd76d5c746e43f9d18bf45c99d9889af618f3b
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-03-27 22:34:38 -0600

    Add Export TXT file & Improve Print function

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,7 @@ import os
 import csv
 
 ## Define path for csv file
-csv_path = os.path.join('Resources','budget_data.csv')
+input_path = os.path.join('Resources','budget_data.csv')
 # print(csv_path)
 
 # Set variable for first month
@@ -22,7 +22,7 @@ max_decrease_value = 0
 max_decrease_month = ""
 
 ## Read CSV file
-with open(csv_path, mode='r', newline='', encoding='utf-8') as csv_file:
+with open(input_path, mode='r', newline='', encoding='utf-8') as csv_file:
     # print(csv_file)
     
     ## Split data on commas
@@ -41,13 +41,13 @@ with open(csv_path, mode='r', newline='', encoding='utf-8') as csv_file:
             month_count += 1
 
             ## Sum Profits/Losses
-            total_pnl += float(row[1])
+            total_pnl += int(row[1])
 
             ## Skip first month since there is no P/L change for the initial value
             if first_month == False:
 
                 ## Compute Profits/Losses change for current month [Change = Current - Last]
-                pnl_change = float(row[1]) - last_pnl
+                pnl_change = int(row[1]) - last_pnl
                 pnl_change_list.append(pnl_change)
 
                 ##
@@ -55,12 +55,13 @@ with open(csv_path, mode='r', newline='', encoding='utf-8') as csv_file:
                     max_increase_value = pnl_change
                     max_increase_month = row[0]
 
+                ##
                 if pnl_change < max_decrease_value:
                     max_decrease_value = pnl_change
                     max_decrease_month = row[0]
 
             ## Store current P/L for next month's analysis
-            last_pnl = float(row[1])
+            last_pnl = int(row[1])
 
             ## Set variable for subsequent months 
             first_month = False
@@ -70,20 +71,33 @@ with open(csv_path, mode='r', newline='', encoding='utf-8') as csv_file:
 avg_change = sum(pnl_change_list) / len(pnl_change_list)
 
 
+## Print Report as a function
 def print_report():
-    ## Print Report
-    print()
-    print("Financial Analysis")
-    print("------------------------------")
-    print (f"Total months: {month_count}")
-    print (f"Total: ${total_pnl:,.0f}")
-    print ("Average Change: ${:,.2f}".format(avg_change))
-    print (f"Greatest Increase in Profits: {max_increase_month} (${max_increase_value:,.0f})")
-    print (f"Greatest Decrease in Profits: {max_decrease_month} (${max_decrease_value:,.0f})")
-    print()
+    return ([
+        "Financial Analysis",
+        "------------------------------",
+        f"Total months: {month_count}",
+        f"Total: ${total_pnl:,}",
+        f"Average Change: ${avg_change:,.2f}",
+        f"Greatest Increase in Profits: {max_increase_month} (${max_increase_value:,})",
+        f"Greatest Decrease in Profits: {max_decrease_month} (${max_decrease_value:,})",
+        ])
 
-print_report()
 
-# str_value = 15.34
-# int_value = int(str_value)
-# print (int_value)
+## EXPORT REPORT TO A TEXT FILE
+
+## Define path for text file
+output_path = os.path.join('Analysis','PyBank_Report.txt')
+# print(output_path)
+
+## Write TXT file
+with open(output_path, mode='w', newline='', encoding='utf-8') as txt_file:
+    # print(txt_file)
+    for line in print_report():
+        txt_file.write(line + "\n")
+
+
+## PRINT REPORT TO THE TERMINAL
+
+for line in print_report():
+    print(line)
