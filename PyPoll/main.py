5029e7a2be8eba44939c04be10218d3fe003006a
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-03-29 23:55:35 -0600

    Add Export TXT file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -39,28 +39,41 @@ with open(input_path, mode='r', newline='', encoding='utf-8') as csv_file:
 ## Print Results as a function
 def print_results():
 
+    ##
     results_header = [
         "Election Results",
         "------------------------------",
         f"Total Votes: {vote_count:,}",
         "------------------------------"
         ]
-
+    ##
     results_candidates = []
     for key, value in results.items():
         results_candidates.append(f"{key}: {value/vote_count:.3%} ({value:,})")
-    
+    ##
     results_winner = [
         "------------------------------",
         f"Winner: {max(results, key=results.get)}",
         "------------------------------"
         ]   
-
+    ##
     return (results_header + results_candidates + results_winner)
 
 
-## PRINT RESULTS TO THE TERMINAL
+## EXPORT REPORT TO A TEXT FILE
 
+## Define path for TXT file
+output_path = os.path.join('Analysis','PyPoll_Results.txt')
+
+## Write TXT file
+with open(output_path, mode='w', newline='', encoding='utf-8') as txt_file:
+    ##
+    for line in print_results():
+        txt_file.write(line + "\n")
+
+
+## PRINT RESULTS TO THE TERMINAL
+##
 for line in print_results():
     print(line)
 
