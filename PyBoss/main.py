7e76ebc7159d124d269aa612736f09c1352e1251
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-04-03 22:16:49 -0600

    Add Export TXT file

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -65,11 +65,9 @@ dob = []
 ssn = []
 state = []
 
-## READ DATA FROM CSV FILE
-
+## IMPORT FROM CSV FILE
 ## Define path for CSV file
 input_path = os.path.join('Resources','employee_data.csv')
-input_path = os.path.join('Resources','example_data.csv')
 
 ## Read CSV file
 with open(input_path, mode='r', newline='', encoding='utf-8') as csv_file:
@@ -77,48 +75,53 @@ with open(input_path, mode='r', newline='', encoding='utf-8') as csv_file:
     ## Split data on commas
     csv_reader = csv.reader(csv_file, delimiter=',')
 
-    ## Get CSV header 
-    csv_header = next(csv_reader)
-    print(f"CSV Header: {csv_header}")
-    
-    ## Loop through CSV rows
+    ## Skip CSV header 
+    next(csv_reader)
+        
+    ## Loop through CSV rows & Change format
     for row in csv_reader:
             
-            ##
+            ## Store 'Emp ID' column as is
             employee_id.append(row[0])
 
-            ##
+            ## Split 'Name' column into separate 'First Name' and 'Last Name' columns
             name_split = row[1].split(sep=' ')
             first_name.append(name_split[0])
             last_name.append(name_split[1])
 
-            ##
+            ## Re-write 'DOB' data into 'MM/DD/YYYY' format
             dob_split = row[2].split(sep='-')
             dob.append(dob_split[1] + "/" + dob_split[2] + "/" + dob_split[0])
 
-            ##
+            ## Re-write 'SSN' data hiding first 5 numbers from view
             ssn_split = row[3].split(sep='-')
             ssn.append("***-**-" + ssn_split[2])
 
-            ##
+            ## Re-write 'State' data into two-letter abbreviations
             state.append(us_state_abbrev[row[4]])
 
-format_conversion = zip (employee_id, first_name, last_name, dob, ssn, state)
+# Set new header (according to new format)
+header = ["Emp ID", "First Name", "Last Name", "DOB", "SSN", "State"]
 
-for line in format_conversion:
-    print (line)
-
-
-
+# Zip all lists together into tuples
+format_conversion = zip(employee_id, first_name, last_name, dob, ssn, state)
 
 
-## EXPORT REPORT TO A CSV FILE
-
+## EXPORT TO CSV FILE
 ## Define path for CSV file
 output_path = os.path.join('Analysis','PyBoss_Conversion.csv')
 
 ## Write CSV file
-# with open(output_path, mode='w', newline='', encoding='utf-8') as csv_file:
+with open(output_path, mode='w', newline='', encoding='utf-8') as csv_file:
+
+    # Initialize csv.writer
+    csv_writer = csv.writer(csv_file, delimiter=',')
+
+    # Write first row (column headers)
+    csv_writer.writerow(header)
+
+    # Write zipped rows (format conversion)
+    csv_writer.writerows(format_conversion)
 
 
 
