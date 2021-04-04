10a9a8a67aca4cc92b6fa227760b6130c6e04943
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-04-04 00:23:48 -0600

    Add Import TXT file

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -5,3 +5,14 @@ import re
 ## Set initial values
 
 
+## IMPORT DATA FROM TXT FILE
+## Define path for TXT file
+input_path = os.path.join('Resources','paragraph_data.txt')
+
+## Read TXT file
+with open(input_path, mode='r', newline='', encoding='utf-8') as txt_file:
+        
+        paragraph = txt_file.read()
+
+
+print(paragraph)
