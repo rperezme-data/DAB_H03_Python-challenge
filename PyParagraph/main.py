f6dc1248622c340fce22c6af27ab07f35a720eb1
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-04-06 22:49:02 -0500

    Add Export TXT file

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -40,3 +40,21 @@ def gen_report():
     
     ## Return Report as a list
     return (report)
+
+
+## EXPORT REPORT TO TXT FILE
+## Define path for TXT file
+output_path = os.path.join('Analysis','PyParagraph_Analysis.txt')
+
+## Write TXT file
+with open(output_path, mode='w', newline='', encoding='utf-8') as txt_file:
+    ## Write line-by-line loop
+    for line in gen_report():
+        txt_file.write(line + "\n")
+
+
+## PRINT REPORT TO TERMINAL
+## Print line-by-line loop
+for line in gen_report():
+    print(line)
+    
